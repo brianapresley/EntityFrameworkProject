e1454b816538495a19cbd548ce49ba23f0143d77
Briana Presley <presley.briana@gmail.com>
2019-08-06 16:26:19 -0400

    Created student DB, added names and queries

--- EntityFrameworkProject/Program.cs
+++ EntityFrameworkProject/Program.cs
@@ -1,9 +1,25 @@
-﻿using System;
+﻿using EntityFrameworkProject.Models;
+using System;
+using System.Linq;
 
 namespace EntityFrameworkProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var context = new AppDbContext();
+
+            //var Students = context.Students.Where(s => s.GPA >= 3.0)
+            //    .OrderBy(s => s.Lastname).ToArray();
+            //var Students = context.Students.OrderBy(s => s.Firstname).ToList();
+
+            var studentAverageSat = context.Students
+                .Average(s => s.SAT);
+            Console.WriteLine($"The average SAT score is {studentAverageSat}");
+            //foreach(var student in Students) {
+            //    Console.WriteLine($"{student.Firstname} {student.Lastname}");
+
+            
+
         }
     }
 }
